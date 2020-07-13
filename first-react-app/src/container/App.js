8bc54898787a667090eecdd3b6f5f4ea27c28cec
joydip-git <34733254+joydip-git@users.noreply.github.com>
2020-07-13 19:15:36 +0530

    end-of-day-1-updates

---
+++ first-react-app/src/container/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+    // JSX
+    // return (
+    //     <p>
+    //         Hello World
+    //     </p>
+    // );
+    return React.createElement('p', null, 'Hello World');
+}
+
+export default App;
---
+++ first-react-app/src/index.js
@@ -0,0 +1,6 @@
+
+
+import ReactDOM from 'react-dom'
+import App from './container/App'
+
+ReactDOM.render(<App />, document.getElementById('root'));
